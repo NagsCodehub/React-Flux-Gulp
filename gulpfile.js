b97d9d6eb54f801bbdb8458dbc906b88237ce0b4
NAGA KARUMANCHI <nagamallik.net@gmail.com>
2017-01-15 20:24:09 -0500

    Created Components,established custom routing,header

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var browserify=require('browserify');// module bundler for JS
 var reactify=require('reactify'); //Transforms React JSX to JS
 var source=require('vinyl-source-stream'); //Use conventional text streams with gulp
 var concat=require('gulp-concat'); //concatenates files
-var lint=require('gulp-eslint');//lint Js files,including JSX
+//var lint=require('gulp-eslint');//lint Js files,including JSX
 
 var config ={
     port:9005,
@@ -17,6 +17,7 @@ var config ={
     paths:{
         html:'./src/*.html',
         js: './src/**/*.js',
+        images:'./src/images/*',
         css:[
             'node_modules/bootstrap/dist/css/bootstrap.min.css',
             'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
@@ -62,15 +63,21 @@ gulp.task('css',function(){
 
 });
 
-gulp.task('lint',function(){
- return gulp.src(config.paths.js)
-              .pipe(lint({config:'eslint.config.json'}))
-              .pipe(lint.format());
+gulp.task('images',function(){
+    gulp.src(config.paths.images)
+    .pipe(gulp.dest(config.paths.dist+'/images'))
+    .pipe(connect.reload());
 });
+
+// gulp.task('lint',function(){
+//  return gulp.src(config.paths.js)
+//               .pipe(lint({config:'eslint.config.json'}))
+//               .pipe(lint.format());
+// });
 
 gulp.task('watch',function(){
 gulp.watch(config.paths.html,['html']);
-gulp.watch(config.paths.js,['js','lint']);
+gulp.watch(config.paths.js,['js']);
 });
 
-gulp.task('default',['html','js','open','css','lint','watch']);
+gulp.task('default',['html','js','open','css','images','watch']);
---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React=require('react');
+
+var Home=React.createClass({
+  render:function(){
+      return (
+          <div className="jumbotron">
+          <h1> App Academy Administration</h1>
+          <p> React,React Router,Flux targeting ES5  for ultra Responsive Apps. </p>
+          </div>
+      )
+  }
+});
+
+module.exports= Home;
